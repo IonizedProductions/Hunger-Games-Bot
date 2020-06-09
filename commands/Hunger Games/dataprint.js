48b4f0d4d7689f6f40439c16d979ca3c8f809d82
Nick <wotk8050@gmail.com>
2020-06-09 16:59:44 +0200

    0.0.3B
    Testing new repo location and messing with dataprint

--- commands/Hunger Games/dataprint.js
+++ commands/Hunger Games/dataprint.js
@@ -13,29 +13,19 @@ class dataprint extends commando.Command {
             group: 'commands',
             memberName: 'dataprint',
             description: 'File Read command for debugging',
-            args: [
-              {
-                key:"file",
-                prompt:"What File would you like to read?",
-                type:"string",
-            },
-            ]
+
         });
 
     }
     async run(message, args) {
 
 
-            if (file == "user4"){
             fs.readFile('"D:/Hunger Games Bot/Data/SignupUsers/user4.txt"', function (err, data) {
           if (err) throw err;
           console.log(data);
           message.channel.send(data);
         });
-      }
-      else {
-        message.reply(", no work sorry my dude");
-      }
+      
 
 
     }
